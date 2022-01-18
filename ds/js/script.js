7c4c28ea1a743a7af14af598478192c6c6c5c7b6
eupho1ria <96536276+eupho1ria@users.noreply.github.com>
2022-01-18 23:35:34 +0600

    Add files via upload

---
+++ ds/js/script.js
@@ -0,0 +1,10 @@
+function changedark()
+{
+	document.getElementById("mode").style.backgroundImage="url(css/back/1.png)";
+}
+
+function changelight()
+{
+	document.getElementById("mode").style.backgroundImage="url(css/back/2.png)";
+}
+
